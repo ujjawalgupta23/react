4436afd16369e4b42673cd8b9200e960330ca44f
Ujjawal Gupta <ujjawalgupta23@gmail.com>
2023-06-06 19:52:51 +0530

    added search

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,25 +1,53 @@
 import { restaurantList } from "../config";
 import RestaurantCard from "./RestaurantCard";
 
+import { useState } from "react";
+
+function filterData(searchText, restaurants) {
+    return restaurants.filter((restaurant) =>
+        restaurant.data.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+}
+
 const Body = () => {
+    const [searchText, setSearchText] = useState("");
+    const [searchClicked, setSearchClicked] = useState(false);
+
+    const [restaurants, setRestaurant] = useState(restaurantList);
+
     return (
-        <div className="restaurant-list">
-            {/* <RestaurantCard restaurant={restaurantList[0]} /> */}
-            {
-                /* <RestaurantCard {...restaurantList[0].data} />
-            <RestaurantCard {...restaurantList[1].data} />
-            <RestaurantCard {...restaurantList[2].data} />
-            <RestaurantCard {...restaurantList[3].data} />
-            <RestaurantCard {...restaurantList[4].data} />
-            <RestaurantCard {...restaurantList[5].data} />
-            <RestaurantCard {...restaurantList[6].data} /> */
-                // This has limitations, suppose i don't know no. of restaurants.
-                // Then I have to use map
-            }
-            {restaurantList.map((restaurant) => (
-                <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
-            ))}
-        </div>
+        <>
+            <div className="search-container">
+                <input
+                    type="text"
+                    className="search-input"
+                    placeholder="Search"
+                    value={searchText}
+                    onChange={(e) => setSearchText(e.target.value)}
+                />
+                {/* <h1>{searchClicked.toString()}</h1> */}
+                <button
+                    className="search-btn"
+                    // onClick={() => setSearchClicked(!searchClicked)}
+                    onClick={() => {
+                        const data = filterData(searchText, restaurants);
+                        setRestaurant(data);
+                    }}
+                >
+                    Search
+                </button>
+            </div>
+            {/* <h1>{searchText}</h1> */}
+            {/* {console.log(searchText)} */}
+            <div className="restaurant-list">
+                {restaurants.map((restaurant) => (
+                    <RestaurantCard
+                        {...restaurant.data}
+                        key={restaurant.data.id}
+                    />
+                ))}
+            </div>
+        </>
     );
 };
 
